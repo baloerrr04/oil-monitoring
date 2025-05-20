bb1b766682758a7183f16c57507fff30a54e50ed
baloerrr <akbaradityah444@gmail.com>
2025-05-20 11:25:36 +0700

    chore: fix chart condition

--- app/(pages)/index.tsx
+++ app/(pages)/index.tsx
@@ -24,13 +24,12 @@ const App: React.FC = () => {
 
   const reversedChartHistory = chartHistory.slice().reverse();
 
- const { handleStatusChange, showWarning } = useWarningToast();
+  const { handleStatusChange, showWarning } = useWarningToast();
 
   useEffect(() => {
-    console.log('Current status:', data?.status, 'Show warning:', showWarning); // Debugging
-    handleStatusChange(data?.status || '');
+    console.log("Current status:", data?.status, "Show warning:", showWarning); // Debugging
+    handleStatusChange(data?.status || "");
   }, [data?.status, handleStatusChange]);
- 
 
   return (
     <View style={{ flex: 1 }}>
@@ -79,161 +78,151 @@ const App: React.FC = () => {
           </View>
         )}
 
-        {history.length > 0 && (
-          <>
-            {/* Grafik RGB */}
-            <View style={styles.chartSection}>
-              <Text style={styles.chartText}>Grafik Perubahan Warna RGB</Text>
-              <LineChart
-                data={reversedChartHistory.map((item, index) => ({
-                  value: item.r,
-                  dataPointText: (index + 1).toString(),
-                }))}
-                color1="red"
-                data2={reversedChartHistory.map((item, index) => ({
-                  value: item.g,
-                  dataPointText: (index + 1).toString(),
-                }))}
-                color2="green"
-                data3={reversedChartHistory.map((item, index) => ({
-                  value: item.b,
-                  dataPointText: (index + 1).toString(),
-                }))}
-                color3="blue"
-                thickness={3}
-                dataPointsColor1="red"
-                dataPointsColor2="green"
-                dataPointsColor3="blue"
-                width={280}
-                backgroundColor="#F0F9FF"
-                hideRules
-                isAnimated
-                yAxisLabelWidth={40}
-                xAxisLabelTextStyle={{ fontSize: 10, color: "#374151" }}
-                yAxisTextStyle={{ fontSize: 10, color: "#374151" }}
-                focusEnabled={true}
-                focusProximity={30} // default biasanya cukup 30
-                delayBeforeUnFocus={1000} // titik fokus akan hilang setelah 1 detik
-                unFocusOnPressOut={false} // biar tetap fokus sampai delay habis
-                focusedDataPointColor="black"
-                focusedDataPointRadius={3}
-                focusedDataPointShape="circle"
-                onFocus={handleFocus}
-              />
+        {/* Grafik RGB */}
+        <View style={styles.chartSection}>
+          <Text style={styles.chartText}>Grafik Perubahan Warna RGB</Text>
+          <LineChart
+            data={reversedChartHistory.map((item, index) => ({
+              value: item?.r || 0,
+              dataPointText: (index + 1).toString(),
+            }))}
+            color1="red"
+            data2={reversedChartHistory.map((item, index) => ({
+              value: item?.g || 0,
+              dataPointText: (index + 1).toString(),
+            }))}
+            color2="green"
+            data3={reversedChartHistory.map((item, index) => ({
+              value: item?.b || 0,
+              dataPointText: (index + 1).toString(),
+            }))}
+            color3="blue"
+            thickness={3}
+            dataPointsColor1="red"
+            dataPointsColor2="green"
+            dataPointsColor3="blue"
+            width={280}
+            backgroundColor="#F0F9FF"
+            hideRules
+            isAnimated
+            yAxisLabelWidth={40}
+            xAxisLabelTextStyle={{ fontSize: 10, color: "#374151" }}
+            yAxisTextStyle={{ fontSize: 10, color: "#374151" }}
+            focusEnabled={true}
+            focusProximity={30} // default biasanya cukup 30
+            delayBeforeUnFocus={1000} // titik fokus akan hilang setelah 1 detik
+            unFocusOnPressOut={false} // biar tetap fokus sampai delay habis
+            focusedDataPointColor="black"
+            focusedDataPointRadius={3}
+            focusedDataPointShape="circle"
+            onFocus={handleFocus}
+          />
 
-              <View style={styles.valuesRow}>
-                <Text style={styles.label}>R: {data ? data?.r : 0}</Text>
-                <Text style={styles.label}>G: {data ? data?.g : 0}</Text>
-                <Text style={styles.label}>B: {data ? data?.b : 0}</Text>
-              </View>
+          <View style={styles.valuesRow}>
+            <Text style={styles.label}>R: {data ? data?.r : 0}</Text>
+            <Text style={styles.label}>G: {data ? data?.g : 0}</Text>
+            <Text style={styles.label}>B: {data ? data?.b : 0}</Text>
+          </View>
 
-              <View style={styles.valuesRow}>
-                <Text style={styles.label}>
-                  Kualitas: {getStatusText(data?.status)}
-                </Text>
-              </View>
-
-              <View style={styles.valuesRow}>
-                <Text style={[styles.label, { fontSize: 12 }]}>
-                  Note: perubahan pada RGB dapat mengindikasi kontaminasi atau
-                  perubahan kualitas
-                </Text>
-              </View>
-            </View>
-
-            {/* Grafik pH */}
-            <View style={styles.chartSection}>
-              <Text style={styles.chartText}>Grafik Tingkat Asam pH</Text>
-              <LineChart
-                data={chartHistory.map((item, index) => ({
-                  value: item.ph,
-                  dataPointText: (index + 1).toString(),
-                }))}
-                color="#F59E0B"
-                thickness={3}
-                dataPointsColor="#FBBF24"
-                width={280}
-                backgroundColor="#FFFBEB"
-                hideRules
-                isAnimated
-                xAxisLabelTextStyle={{ fontSize: 10, color: "#374151" }}
-                yAxisTextStyle={{ fontSize: 10, color: "#374151" }}
-                focusEnabled={true}
-                focusProximity={30} // default biasanya cukup 30
-                delayBeforeUnFocus={1000} // titik fokus akan hilang setelah 1 detik
-                unFocusOnPressOut={false} // biar tetap fokus sampai delay habis
-                focusedDataPointColor="black"
-                focusedDataPointRadius={3}
-                focusedDataPointShape="circle"
-                onFocus={handleFocus}
-              />
-              <Text style={styles.label}>pH: {data ? data.ph : 0}</Text>
-
-              <View style={styles.valuesRow}>
-                <Text style={styles.label}>
-                  Kualitas: {getStatusText(data?.status)}
-                </Text>
-              </View>
-
-              <View style={styles.valuesRow}>
-                <Text style={[styles.label, { fontSize: 12 }]}>
-                  Note: pH dibawah normal dapat menunjukkan oksidasi
-                </Text>
-              </View>
-            </View>
-
-            {/* Grafik LDR */}
-            <View style={styles.chartSection}>
-              <Text style={styles.chartText}>Grafik Intensitas Cahaya LDR</Text>
-              <LineChart
-                data={chartHistory.map((item, index) => ({
-                  value: item.ldr,
-                  dataPointText: (index + 1).toString(),
-                }))}
-                color="#EF4444"
-                thickness={3}
-                dataPointsColor="black"
-                width={280}
-                backgroundColor="#FEF2F2"
-                hideRules
-                isAnimated
-                xAxisLabelTextStyle={{ fontSize: 10, color: "#374151" }}
-                yAxisTextStyle={{ fontSize: 10, color: "#374151" }}
-                dataPointsColor1="red"
-                focusEnabled={true}
-                focusProximity={30} // default biasanya cukup 30
-                delayBeforeUnFocus={1000} // titik fokus akan hilang setelah 1 detik
-                unFocusOnPressOut={false} // biar tetap fokus sampai delay habis
-                focusedDataPointColor="black"
-                focusedDataPointRadius={3}
-                focusedDataPointShape="circle"
-                onFocus={handleFocus}
-              />
-              <Text style={styles.label}>LDR: {data ? data?.ldr : 0}</Text>
-
-              <View style={styles.valuesRow}>
-                <Text style={styles.label}>
-                  Kualitas: {getStatusText(data?.status)}
-                </Text>
-              </View>
-
-              <View style={styles.valuesRow}>
-                <Text style={[styles.label, { fontSize: 12 }]}>
-                  Note: Nilai LDR tinggi menandakan minyak telah keruh akibat
-                  penggunaan berulang
-                </Text>
-              </View>
-            </View>
-          </>
-        )}
-
-        {chartHistory.length === 0 && (
-          <View style={styles.noDataContainer}>
-            <Text style={styles.noDataText}>
-              Tidak ada data riwayat untuk grafik
+          <View style={styles.valuesRow}>
+            <Text style={styles.label}>
+              Kualitas: {getStatusText(data?.status || "Belum ada data")}
             </Text>
           </View>
-        )}
+
+          <View style={styles.valuesRow}>
+            <Text style={[styles.label, { fontSize: 12 }]}>
+              Note: perubahan pada RGB dapat mengindikasi kontaminasi atau
+              perubahan kualitas
+            </Text>
+          </View>
+        </View>
+
+        {/* Grafik pH */}
+        <View style={styles.chartSection}>
+          <Text style={styles.chartText}>Grafik Tingkat Asam pH</Text>
+          <LineChart
+            data={reversedChartHistory.map((item, index) => ({
+              value: item?.ph || 0,
+              dataPointText: (index + 1).toString(),
+            }))}
+            color="#F59E0B"
+            thickness={3}
+            dataPointsColor="#FBBF24"
+            width={280}
+            backgroundColor="#FFFBEB"
+            hideRules
+            isAnimated
+            xAxisLabelTextStyle={{ fontSize: 10, color: "#374151" }}
+            yAxisTextStyle={{ fontSize: 10, color: "#374151" }}
+            focusEnabled={true}
+            focusProximity={30} // default biasanya cukup 30
+            delayBeforeUnFocus={1000} // titik fokus akan hilang setelah 1 detik
+            unFocusOnPressOut={false} // biar tetap fokus sampai delay habis
+            focusedDataPointColor="black"
+            focusedDataPointRadius={3}
+            focusedDataPointShape="circle"
+            onFocus={handleFocus}
+          />
+          <Text style={styles.label}>pH: {data ? data.ph : 0}</Text>
+
+          <View style={styles.valuesRow}>
+            <Text style={styles.label}>
+              Kualitas: {getStatusText(data?.status || "Belum ada data")}
+            </Text>
+          </View>
+
+          <View style={styles.valuesRow}>
+            <Text style={[styles.label, { fontSize: 12 }]}>
+              Note: pH dibawah normal dapat menunjukkan oksidasi
+            </Text>
+          </View>
+        </View>
+
+        {/* Grafik LDR */}
+        <View style={styles.chartSection}>
+          <Text style={styles.chartText}>Grafik Intensitas Cahaya LDR</Text>
+          <LineChart
+            data={reversedChartHistory.map((item, index) => ({
+              value: item?.ldr || 0,
+              dataPointText: (index + 1).toString(),
+            }))}
+            color="#EF4444"
+            thickness={3}
+            dataPointsColor="black"
+            width={280}
+            backgroundColor="#FEF2F2"
+            hideRules
+            isAnimated
+            xAxisLabelTextStyle={{ fontSize: 10, color: "#374151" }}
+            yAxisTextStyle={{ fontSize: 10, color: "#374151" }}
+            dataPointsColor1="red"
+            focusEnabled={true}
+            focusProximity={30} // default biasanya cukup 30
+            delayBeforeUnFocus={1000} // titik fokus akan hilang setelah 1 detik
+            unFocusOnPressOut={false} // biar tetap fokus sampai delay habis
+            focusedDataPointColor="black"
+            focusedDataPointRadius={3}
+            focusedDataPointShape="circle"
+            onFocus={handleFocus}
+          />
+          <Text style={styles.label}>LDR: {data ? data?.ldr : 0}</Text>
+
+          <View style={styles.valuesRow}>
+            <Text style={styles.label}>
+              Kualitas: {getStatusText(data?.status || "Belum ada data")}
+            </Text>
+          </View>
+
+          <View style={styles.valuesRow}>
+            <Text style={[styles.label, { fontSize: 12 }]}>
+              Note: Nilai LDR tinggi menandakan minyak telah keruh akibat
+              penggunaan berulang
+            </Text>
+          </View>
+        </View>
+
+        
 
         {!data && (
           <View style={styles.noDataContainer}>
@@ -246,6 +235,14 @@ const App: React.FC = () => {
           handleDeleteRiwayat={handleDeleteRiwayat}
         />
 
+        {chartHistory.length === 0 && (
+          <View style={styles.noDataContainer}>
+            <Text style={styles.noDataText}>
+              Tidak ada data riwayat untuk grafik
+            </Text>
+          </View>
+        )}
+
         <View style={styles.logoutContainer}>
           <Button title="Logout" onPress={handleLogout} />
         </View>
@@ -253,10 +250,10 @@ const App: React.FC = () => {
 
       {focusedValue !== null && <FocusToast value={focusedValue} />}
 
-        <WarningToast
-          message="Minyak anda sudah beberapa kali pakai, segera ganti."
-          visible={showWarning}
-        />
+      <WarningToast
+        message="Minyak anda sudah beberapa kali pakai, segera ganti."
+        visible={showWarning}
+      />
     </View>
   );
 };
